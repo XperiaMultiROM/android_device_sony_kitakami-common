830561c089186724e91f29a40c7d04d2b0aa831e
Christian Oder <myself5@carbonrom.org>
2016-03-08 21:52:31 +0100

    dirty hacks for Omni,TWRP and MultiROM

---
+++ PlatformConfigOmni.mk
@@ -0,0 +1,27 @@
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+include device/sony/kitakami/PlatformConfig.mk
+
+#MultiROM config. MultiROM also uses parts of TWRP config
+TARGET_RECOVERY_IS_MULTIROM := true
+MR_INPUT_TYPE := type_b
+MR_INIT_DEVICES := device/sony/kitakami/multirom/init_devices.c
+MR_USE_QCOM_OVERLAY := true
+MR_QCOM_OVERLAY_HEADER := device/sony/kitakami/multirom/framebuffer_qcom_overlay.h
+MR_KEXEC_DTB := true
+MR_FSTAB := device/sony/kitakami/rootdir/twrp.fstab
+MR_USE_MROM_FSTAB := true
+MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+MR_PIXEL_FORMAT := "RGBX_8888"
